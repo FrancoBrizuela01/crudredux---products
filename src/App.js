aa83e1a092635875b99c3d29ced10a21b6078a06
FrancoBrizuela01 <franco-alexis16@hotmail.com>
2022-02-13 21:28:53 -0300

    create json-server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Product from "./components/Product";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NewProduct from "./components/NewProduct";
+import EditProduct from "./components/EditProduct";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <div className="container mt-5">
+        <Routes>
+          <Route exact path="/" element={<Product />} />
+          <Route excat path="/product/new" element={<NewProduct />} />
+          <Route excat path="/product/edit/:id" element={<EditProduct />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+//para ejecutar la bd: npx json-server .\db.json --port 4000
+//ver 295 queda ver
